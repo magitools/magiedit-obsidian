6c9fc7e3bffbc064a64298a2286ba92afb100e20
Matteo Gassend <mgassend@gmail.com>
2024-10-22 11:19:11 +0200

    add: updated readme

--- README.md
+++ README.md
@@ -1,96 +1,22 @@
-# Obsidian Sample Plugin
+# Magiedit Obsidian
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+> Official plugin to interact with [magiedit](https://magiedit.magitools.app)
 
-This project uses TypeScript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in TypeScript Definition format, which contains TSDoc comments describing what it does.
+## Usage
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+- Install It
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+## Configuration
 
-## First time developing plugins?
+There are two settings you should be aware of when using this plugin:
 
-Quick starting guide for new plugin devs:
+- url: the url of the instance of magiedit you're trying to connect to (defaults to the origin instance at magiedit.magitools.app)
+- api_key: the api key you will use to interact with the magiedit api (you can create one from the magiedit web application)
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+## Commands
 
-## Releasing new releases
+This plugins introduces one command: **Publish Current File**.
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+### Publish Current File
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
-
-## Adding your plugin to the community plugin list
-
-- Check the [plugin guidelines](https://docs.obsidian.md/Plugins/Releasing/Plugin+guidelines).
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
-
-## How to use
-
-- Clone this repo.
-- Make sure your NodeJS is at least v16 (`node --version`).
-- `npm i` or `yarn` to install dependencies.
-- `npm run dev` to start compilation in watch mode.
-
-## Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+This command will retrieve all your configured publishers, allow you to choose which publisher you'd like to use, and then send a POST request to magiedit's API with the selected publishers and the content of the currently opened file to be published.
--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, SuggestModal } from 'obsidian';
+import { ofetch } from "ofetch";
+
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	url: string;
+	api_key: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	url: 'https://magiedit.magitools.app',
+	api_key: ""
 }
 
 export default class MyPlugin extends Plugin {
@@ -23,56 +27,26 @@ export default class MyPlugin extends Plugin {
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new MagieditSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.addCommand({
+			id: 'magiedit-publish',
+			name: 'Publish Selected Content',
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				const { publishers } = await ofetch(`${this.settings.url}/api/publishers`, {
+					headers: {
+						'Authorization': `Bearer ${this.settings.api_key}`
+					}
+				})
+				new PublisherSelectModal(this.app, publishers, (ev: Record<string, any>) => {
+					new Notice(`this is the list of publishers selected : ${Object.keys(ev).join(',')}`)
+					const title = editor
+					const value = editor.getValue()
+					console.log(value)
+				}).open()
+			}
+		})
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -91,23 +65,48 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+interface Publisher {
+	id: number;
+	name: string;
+}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+//TODO make the elements deletable
+//Check if item is already in list
+//To disable duplicates
+export class PublisherSelectModal extends Modal {
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	constructor(app: App, publishers: Array<Publisher>, onSubmit: (data: Record<string, string>) => void) {
+		super(app)
+		const selectedPublishers = {}
+		const itemList = this.contentEl.createDiv()
+
+		new Setting(this.contentEl)
+			.addDropdown((dropdown) => {
+				dropdown.addOption(-1, "Select at least a publisher")
+				publishers.forEach((el) => {
+					dropdown.addOption(String(el.id), el.name)
+				})
+				dropdown.onChange((value) => {
+					const container = itemList.createEl('div')
+					container.createEl('div', { text: publishers.find((e) => e.id == value)?.name })
+					selectedPublishers[value] = container
+				})
+			})
+		new Setting(this.contentEl)
+			.addButton((btn) => {
+				btn
+					.setButtonText('submit')
+					.setCta()
+					.onClick(() => {
+						onSubmit(selectedPublishers)
+						this.close()
+					})
+			})
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
+
+class MagieditSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -116,19 +115,30 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Magiedit Url')
+			.setDesc('change this if you self-host magiedit')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter the url of your magiedit instance')
+				.setValue(this.plugin.settings.url)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.url = value;
 					await this.plugin.saveSettings();
 				}));
+		new Setting(containerEl)
+			.setName('API Key')
+			.setDesc('Change to the API Key you created on your magiedit instance')
+			.addText(text => text
+				.setPlaceholder('Enter the api key you created  on your magiedit instance')
+				.setValue(this.plugin.settings.api_key)
+				.onChange(async (value) => {
+					this.plugin.settings.api_key = value;
+					await this.plugin.saveSettings();
+				})
+			)
 	}
 }
